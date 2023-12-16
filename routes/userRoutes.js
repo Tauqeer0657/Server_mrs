dec19d629fa2a05aec11c307cde9337603f22834
nawaid <iqbalnawaid15@gmail.com>
2023-12-16 11:47:30 +0530

    updated routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,38 @@
+// routes/userRoutes.js
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const auth = require('../middleware/auth');
+const multer = require('multer');
+const path = require('path');
+router.use(express.static('public'));
+
+
+// for saving image using registration form 
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'../public/userImages'))
+    },
+    filename: function (req, file, cb) {
+      const name = Date.now() + '-' + file.originalname;
+      cb(null, name)
+    }
+  })
+  
+  const upload = multer({ storage: storage });
+
+// Login
+router.post('/login', userController.loginUser);
+
+// Registration
+router.post('/register',upload.single('image'), userController.registerUser);
+router.get('/registrations', userController.getAllRegistrations);
+
+// Logout from single device
+router.get('/logout', auth, userController.logoutUser);
+
+// Logout from all devices
+router.get('/logout/all', auth, userController.logoutAllDevices);
+
+module.exports = router;
